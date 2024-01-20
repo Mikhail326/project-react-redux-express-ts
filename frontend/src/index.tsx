33f27f5661431be3677129c86ea843ba4998bcd7
Mikhail326 <misha.stelmakh1995@gmail.com>
2024-01-20 13:19:04 +0300

    Preparation routers

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -2,9 +2,26 @@ import React from 'react';
 import { createRoot } from 'react-dom/client';
 import { Provider } from 'react-redux';
 import { store } from './app/store';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import { RouterProvider, createBrowserRouter } from 'react-router-dom';
+import { Paths } from './paths'
+import { Login } from './pages/login/login';
+import { Register } from './pages/register/register';
 import './index.css';
+
+const router = createBrowserRouter([
+  {
+    path: Paths.home,
+    element: <h1>Home</h1>
+  },
+  {
+    path: Paths.login,
+    element: <Login />
+  },
+  {
+    path: Paths.register,
+    element: <Register />
+  }
+])
 
 const container = document.getElementById('root')!;
 const root = createRoot(container);
@@ -12,12 +29,7 @@ const root = createRoot(container);
 root.render(
   <React.StrictMode>
     <Provider store={store}>
-      <App />
+      <RouterProvider router={router}/>
     </Provider>
   </React.StrictMode>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
---
+++ frontend/src/paths.ts
@@ -0,0 +1,9 @@
+export const Paths = {
+  home: '/',
+  login: 'login',
+  register: 'register',
+  status: '/status',
+  employee: '/employee',
+  employeeAdd: '/employee/add',
+  employeeEdit: '/employee/edit'
+} as const
